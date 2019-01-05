43c87669b6b0b5c73fc4765857db360f07c87f28
Bill Li <me@billxinli.com>
2019-01-05 18:09:16 -0500

    wip

--- src/lib/copy-and-link.js
+++ src/lib/copy-and-link.js
@@ -7,6 +7,7 @@ const inquirer = require('inquirer')
 const globCallback = require('glob')
 
 const homeLabConfig = require('./get-config')
+const searchMovie = require('./search-movies')
 
 const asyncLstat = util.promisify(fs.lstat)
 const glob = util.promisify(globCallback)
@@ -46,6 +47,33 @@ function isNotSample (stats, sampleSize) {
   return true
 }
 
+async function resolveNames (movies) {
+
+  let result = Promise.resolve()
+  movies.forEach((movie) => {
+    result = result.then(async () => {
+
+      console.log('Processing: ', movie.path)
+      const question = [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Movie name',
+          default: async () => searchMovie(movie.path),
+          validate: (value) => value.length.trim() > 0
+        }
+      ]
+
+      const answer = await inquirer.prompt(question)
+
+      console.log(answer)
+      return answer
+    })
+  })
+  return result
+
+}
+
 async function copyAndLink (currentPath, offset, limit, sampleSize) {
 
   const mf = await glob('./**/*.+(' + ['mkv'].join('|') + ')', { cwd: currentPath })
@@ -55,10 +83,20 @@ async function copyAndLink (currentPath, offset, limit, sampleSize) {
   const statMf = await Promise.all(absPathMf.map(getFileStats))
 
   const nonSymlinkMf = statMf.filter(isNotSymlink)
-  console.log({ nonSymlinkMf })
-  const nonSamples = nonSymlinkMf.filter((stats) => isNotSample(stats, sampleSize))
-  console.log({ nonSamples }, nonSamples.length)
 
+  const nonSamples = nonSymlinkMf.filter((stats) => isNotSample(stats, sampleSize))
+
+  const limitMf = nonSamples.slice(offset, offset + limit)
+
+  const nameMf = await resolveNames(limitMf)
+
+  // var result = Promise.resolve()
+  // tasks.forEach(task => {
+  //   result = result.then(() => task())
+  // })
+  // return result
+
+  console.log(limitMf)
 //  const movieMountsInfo = await parseMovieMounts(movieMounts)
 
   return arguments
