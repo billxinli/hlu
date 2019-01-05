224c9ac6e56cf66f8b120f136b8522ea96a32708
Bill Li <me@billxinli.com>
2019-01-05 14:23:00 -0500

    wip

--- src/lib/copy-and-link.js
+++ src/lib/copy-and-link.js
@@ -1,7 +1,14 @@
+const fs = require('fs-extra')
+
 const disk = require('diskusage')
+const path = require('path')
+const util = require('util')
+const inquirer = require('inquirer')
+const globCallback = require('glob')
 
 const homeLabConfig = require('./get-config')
 
+const glob = util.promisify(globCallback)
 const { movieMounts } = homeLabConfig
 
 async function parseMovieMounts (mounts) {
@@ -15,7 +22,27 @@ async function parseMovieMounts (mounts) {
   return results
 }
 
+const asyncLstat = util.promisify(fs.lstat)
+
 async function copyAndLink (currentPath, offset, limit, sampleSize) {
+
+  const mf = await glob('./**/*.+(' + ['mkv'].join('|') + ')', { cwd: currentPath })
+
+  const filteredMf = mf.slice(offset, offset + limit)
+
+  const absPathMf = filteredMf.map(file => path.join(currentPath, file))
+
+  const statMf = await Promise.all(absPathMf.map(async (mfPath) => {
+    const result = await asyncLstat(mfPath)
+    return {
+      path: mfPath,
+      symlink: result.isSymbolicLink(),
+      size: result.size,
+      result
+    }
+  }))
+
+  console.log(statMf)
 
   const movieMountsInfo = await parseMovieMounts(movieMounts)
 
