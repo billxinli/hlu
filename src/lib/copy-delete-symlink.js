5a7c117bee4a0da7f7033891c5dab5a54afb4570
Bill Li <me@billxinli.com>
2019-01-05 19:11:09 -0500

    wip

---
+++ src/lib/copy-delete-symlink.js
@@ -0,0 +1,73 @@
+const fs = require('fs-extra')
+const path = require('path')
+const util = require('util')
+
+const nameCleaner = require('../../homelab/lib/filename-cleaner')
+
+const asyncStat = util.promisify(fs.stat)
+const asyncMkdirs = util.promisify(fs.mkdirs)
+const asyncCopy = util.promisify(fs.copy)
+const asyncUnlink = util.promisify(fs.unlink)
+const asyncSymlink = util.promisify(fs.symlink)
+
+async function ensureDirectoryDoesNotExist (path) {
+  try {
+    await asyncStat(path)
+    return false
+  } catch (err) {
+    return true
+  }
+}
+
+async function makeNewAssetDirectory (path) {
+
+  console.log(`creating ${path}`)
+  return Promise.resolve(null)
+
+  // return await asyncMkdirs(path)
+}
+
+async function copyAsset (from, to) {
+
+  console.log(`copying ${from} -> ${to}`)
+  return Promise.resolve(null)
+  // return await asyncCopy(from, to)
+}
+
+async function deleteAsset (path) {
+
+  console.log(`deleting ${path}`)
+  return Promise.resolve(null)
+  // return await asyncUnlink(path)
+}
+
+async function createSymlink (target, path) {
+
+  console.log(`symlink ${target} ${path}`)
+  return Promise.resolve(null)
+  // return await asyncSymlink(target, path)
+}
+
+async function copyDeleteSymlink (source, target, folder, name) {
+  const cleanName = nameCleaner(name)
+
+  const newPath = [target, folder, cleanName].join('/')
+  const newFile = [target, folder, cleanName, [cleanName, path.extname(source)].join('')].join('/')
+
+  const doesNotExist = await ensureDirectoryDoesNotExist(newPath)
+
+  if (!doesNotExist) {
+    throw new Error(`${newPath} already exists.`)
+  }
+
+  await makeNewAssetDirectory(newPath)
+
+  await copyAsset(source, newFile)
+
+  await deleteAsset(source)
+
+  await createSymlink(newFile, source)
+
+}
+
+module.exports = copyDeleteSymlink
